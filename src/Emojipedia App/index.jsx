b662b5914436ed067733224c5b2c29ed72c99d5d
Vishnu-Devhub <vishnuvishnu04021@gmail.com>
2026-07-01 22:05:52 +1000

    Updated Emojipedia App with new feature (Total Count)

--- src/Emojipedia App/index.jsx
+++ src/Emojipedia App/index.jsx
@@ -6,12 +6,19 @@ import Card from "./components/cards.jsx";
 function App() {
     return (
         <div className="grid-layout">
-            {contents.map((emoji, index) => (
-                <Card key={index} {...emoji}/>
-            ))}
+                {contents.map((emoji, index) => (
+                    <Card key={index} {...emoji}/>
+                ))}
         </div>
     );
 }
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
+
+const headerCount = createRoot(document.getElementById("header-count"));
+headerCount.render(
+    <div className="count">
+        <p>Emojis: {contents.length}</p>
+    </div>
+);
