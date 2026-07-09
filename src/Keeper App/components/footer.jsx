76097dff9aac0f1c9375b19bc46662b1c0c659eb
Vishnu-Devhub <vishnuvishnu04021@gmail.com>
2026-07-09 11:03:13 +1000

    Updated Keeper app with new styles

--- src/Keeper App/components/footer.jsx
+++ src/Keeper App/components/footer.jsx
@@ -1,6 +1,5 @@
 function Footer() {
     const year = new Date().getFullYear();
-
     return (
         <footer>
             <p>Copyright © {year}</p>
--- src/Keeper App/index.jsx
+++ src/Keeper App/index.jsx
@@ -1,16 +1,35 @@
 import React from "react";                          // ✅ no unused useState
 import { useState, useEffect } from "react";        // ✅ useEffect added
-import { createRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client"; 
 import Note from "./components/note.jsx";
 import Footer from "./components/footer.jsx";
 import notes from "./components/notes-data.js";
 
 function App() {
+    const [newHeading, setNewHeading] = useState("");
+    const [newContent, setNewContent] = useState("");
+
+    function newNote(e) {
+        console.log("New Note Creating...");
+        if(e.target === "headingInput"){
+            setNewHeading(e.target.value);
+        }else {
+            setNewContent(e.target.value);
+        }
+    };
+
     return (
-        <div>
-            {notes.map((note) => (
-                <Note key={note.id} heading={note.heading} content={note.content} />
-            ))}
+        <div className= "main-layout">
+            <div className= "input-container">
+                <input className="headingInput" placeholder="Note Heading:"/>
+                <input className="contentInput" type="paragraph" placeholder="Note Content:"/>
+                <button className="add-button" onClick = {newNote}>Add</button>
+            </div>
+            <div className= "grid-layout">
+                {notes.map((note) => (
+                    <Note key={note.id} heading={note.heading} content={note.content} />
+                ))}
+            </div>
             <Footer />
         </div>
     );
